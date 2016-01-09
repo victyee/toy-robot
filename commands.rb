e7f14efa9272f74d66ac9621c538600c722265bd
Victor Yee <victyee@gmail.com>
2016-01-09 11:48:36 +1100

    adds all other valid commands

--- commands.rb
+++ commands.rb
@@ -7,8 +7,102 @@ def check_command
 		puts "You need to place the robot first!"
 		prompt_command
 	end
+
+	# All available valid commands
+	if @command_split[0] == "PLACE"
+		place
+	elsif @command_split[0] == "REPORT"
+		report
+	elsif @command_split[0] == "MOVE"
+		move
+	elsif @command_split[0] == "LEFT"
+		left
+	elsif @command_split[0] == "RIGHT"
+		right
+	else
+		put "Invalid command. Try again"
+		prompt_command
+	end
 end
 
+
+# MOVE command
+def move
+	if @robot_facing == "NORTH"
+		unless @robot_y_axis.to_i == 4
+			@robot_y_axis = @robot_y_axis.to_i + 1
+		else
+			puts "Can't move. Will drop off the table"
+		end
+	elsif @robot_facing == "SOUTH"
+		unless @robot_y_axis.to_i == 0
+			@robot_y_axis = @robot_y_axis.to_i - 1
+		else
+			puts "Can't move. Will drop off the table"
+		end
+	elsif @robot_facing == "EAST"
+		unless @robot_x_axis.to_i == 4
+			@robot_x_axis = @robot_x_axis.to_i + 1
+		else
+			puts "Can't move. Will drop off the table"
+		end
+	elsif @robot_facing == "WEST"
+		unless @robot_x_axis.to_i == 0
+			@robot_x_axis = @robot_x_axis.to_i - 1
+		else
+			puts "Can't move. Will drop off the table"
+		end
+	end
+	prompt_command
+end
+
+
+# LEFT command
+def left
+	if @robot_facing == "NORTH"
+		@robot_facing = "WEST"
+	elsif @robot_facing == "SOUTH"
+		@robot_facing = "EAST"
+	elsif @robot_facing == "EAST"
+		@robot_facing = "NORTH"
+	elsif @robot_facing == "WEST"
+		@robot_facing = "SOUTH"
+	end
+	prompt_command
+end
+
+
+# RIGHT command
+def right
+	if @robot_facing == "NORTH"
+		@robot_facing = "EAST"
+	elsif @robot_facing == "SOUTH"
+		@robot_facing = "WEST"
+	elsif @robot_facing == "EAST"
+		@robot_facing = "SOUTH"
+	elsif @robot_facing == "WEST"
+		@robot_facing = "NORTH"
+	end
+	prompt_command
+end
+
+
+# PLACE command
+def place
+	if validX && validY && validF
+		@hasRobot = true
+		robot(@command_split[1], @command_split[2], @command_split[3])
+	else
+		prompt_command
+	end
+end
+
+
+#  REPORT command
+def report
+	puts "Output: #{@robot_x_axis},#{@robot_y_axis},#{@robot_facing}"
+	prompt_command
+end
 
 # Initiates user's input
 def prompt_command
--- simulator.rb
+++ simulator.rb
@@ -1,7 +1,7 @@
 require_relative 'commands.rb'
 require_relative 'robot.rb'
 
-
+# Initial settings
 @hasRobot = false
 @robotPosition = ['NORTH', 'SOUTH', 'EAST', 'WEST']
 @maxX = 4
