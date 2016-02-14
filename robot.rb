2ce8c28e81e6002547b013ecfb4e240103c746c5
Victor Yee <victyee@gmail.com>
2016-02-14 14:11:55 +1100

    adds class robot and class simulator

--- robot.rb
+++ robot.rb
@@ -1,7 +1,85 @@
-# Sets robot's X and Y axis and position
-def robot(x, y, f)
-	@robot_x_axis = x
-	@robot_y_axis = y
-	@robot_facing = f
-	check_testmode
+class Robot
+  attr_reader :place
+
+  def initialize(x,y,f)
+    @has_robot = true
+
+    @x_axis = x.to_i
+    @y_axis = y.to_i
+    @f_facing = f
+
+    @robot_positions = ['NORTH', 'SOUTH', 'EAST', 'WEST']
+  end
+
+  def position
+    return "Output: #{@x_axis},#{@y_axis},#{@f_facing}"
+  end
+
+  # REPORT command
+  def report
+    puts "Output: #{@x_axis},#{@y_axis},#{@f_facing}"
+  end
+
+  # MOVE command
+  def move
+    facing = @f_facing
+    case facing 
+    when "NORTH"
+      unless @y_axis >= 4
+        @y_axis += 1
+      else
+        puts "Can't move. Will drop off the table"
+      end
+    when "SOUTH"
+      unless @y_axis <= 0
+        @y_axis -= 1
+      else
+        puts "Can't move. Will drop off the table"
+      end
+    when "EAST"
+      unless @x_axis >= 4
+        @x_axis += 1
+      else
+        puts "Can't move. Will drop off the table"
+      end
+    when "WEST"
+      unless @x_axis <= 0
+        @x_axis -= 1
+      else
+        puts "Can't move. Will drop off the table"
+      end
+    end
+  end
+
+  # LEFT command
+  def left
+    facing = @f_facing
+    case facing 
+    when "NORTH"
+      @f_facing = "WEST"
+    when "SOUTH"
+      @f_facing = "EAST"
+    when "EAST"
+      @f_facing = "NORTH"
+    when "WEST"
+      @f_facing = "SOUTH"
+    end
+    return @robot
+  end
+
+  # RIGHT command
+  def right
+    facing = @f_facing
+    case facing 
+    when "NORTH"
+      @f_facing = "EAST"
+    when "SOUTH"
+      @f_facing = "WEST"
+    when "EAST"
+      @f_facing = "SOUTH"
+    when"WEST"
+      @f_facing = "NORTH"
+    end
+    return @robot
+  end
 end
--- simulator.rb
+++ simulator.rb
@@ -1,15 +1,107 @@
-require_relative 'commands.rb'
-require_relative 'tests.rb'
-require_relative 'robot.rb'
+require_relative 'robot'
+
+class Simulator
+  def initialize
+    @has_robot = false
+    @max_x = 4
+    @min_x = 0
+    @max_y = 4
+    @min_y = 0
+    @robot_positions = ['NORTH', 'SOUTH', 'EAST', 'WEST']
+  end
+
+  def prompt_command
+    if @has_robot
+      print "Input command > "
+    else
+      print "Place your robot > "
+    end
+    @command = $stdin.gets.chomp
+    check_command
+  end
+
+  def check_command
+    @command_split = @command.split(/\W+/)
+    command = @command_split.first
+    case command
+    when "PLACE"
+      if valid_x && valid_y && valid_f
+        @has_robot = true
+        @robot = Robot.new(@command_split[1], @command_split[2], @command_split[3])
+      end
+    when "REPORT"
+      check_has_robot
+      @robot.report
+    when "MOVE"
+      check_has_robot
+      @robot.move
+    when "LEFT"
+      check_has_robot
+      @robot.left
+    when "RIGHT"
+      check_has_robot
+      @robot.right
+    else
+      puts "ERROR: Invalid command, please try again"
+    end
+    prompt_command
+  end
+
+  # Checks if entered X and Y axis are numbers
+  def is_numeric?(obj) 
+    obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
+  end
+
+  # Verify if X axis is valid
+  def valid_x
+    axis = @command_split[1]
+    if is_numeric?(axis)
+      if axis.to_i < @min_x || axis.to_i > @max_x
+        puts "ERROR: X axis is out of range! "
+        return false
+      else
+        return true
+      end
+    else
+      puts "ERROR: Please enter a valid X axis"
+      return false
+    end
+  end
+
+  # Verifies if Y axis is valid
+  def valid_y
+    axis = @command_split[2]
+    if is_numeric?(axis)
+      if axis.to_i < @min_y || axis.to_i > @max_y
+        puts "ERROR: Y axis is out of range! "
+        return false
+      else
+        return true
+      end
+    else
+      puts "ERROR: Please enter a valid Y axis"
+      return false
+    end
+  end
+
+  # Verifies if Position is valid
+  def valid_f
+    if @robot_positions.include? @command_split[3]
+      return true
+    else
+      puts "ERROR: Please enter a valid position"
+      return false
+    end
+  end
+
+  def check_has_robot
+    if !@has_robot
+      puts "ERROR: Place robot first"
+      prompt_command
+    end
+  end
+end
 
 
-# Initial settings
-@test_mode = false
-@has_robot = false
-@robot_positions = ['NORTH', 'SOUTH', 'EAST', 'WEST']
-@max_x = 4
-@max_y = 4
-
-
-# Initiates the robot simulator
-prompt_command
+@session = Simulator.new
+@session.prompt_command
